bf0c87656bdcbb157675cdf86418fe7e4b6ebdef
Chandan Singh <chandan_singh@berkeley.edu>
2021-10-09 14:27:04 -0700

    remove autogluon pkls

--- mrules/__init__.py
+++ mrules/__init__.py
@@ -5,7 +5,7 @@ import os
 from os.path import join as oj
 
 MRULES_PATH = os.path.dirname(os.path.abspath(__file__))
-REPO_PATH = oj(MRULES_PATH, '..')
+REPO_PATH = os.path.dirname(MRULES_PATH)
 DATA_PATH = oj(REPO_PATH, 'data')
 PROJECTS_PATH = oj(MRULES_PATH, 'projects')
 AUTOGLUON_CACHE_PATH = oj(DATA_PATH, 'autogluon_cache')
