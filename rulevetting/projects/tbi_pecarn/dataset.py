8d46288283c5f13fe36f9c8021f0a4881b702246
Andrej Leban <andrejleban@gmail.com>
2021-11-26 22:05:28 -0800

    Added documentation to dataset.py, small bugfixes

--- rulevetting/projects/tbi_pecarn/dataset.py
+++ rulevetting/projects/tbi_pecarn/dataset.py
@@ -15,6 +15,22 @@ from rulevetting.templates.dataset import DatasetTemplate
 
 class Dataset(DatasetTemplate):
     def clean_data(self, data_path: str = rulevetting.DATA_PATH, **kwargs) -> pd.DataFrame:
+        """
+        Convert the raw data files into a pandas dataframe.
+        Dataframe keys should be reasonable (lowercase, underscore-separated).
+        Data types should be reasonable.
+
+        Params
+        ------
+        data_path: str, optional
+            Path to all data files
+        kwargs: dict
+            Dictionary of hyperparameters specifying judgement calls
+
+        Returns
+        -------
+        cleaned_data: pd.DataFrame
+        """
 
         # raw data path
         raw_data_path = oj(data_path, self.get_dataset_id(), 'raw')
@@ -32,7 +48,24 @@ class Dataset(DatasetTemplate):
 
         return df
 
+    # TODO: don't copy the df @ each step once sure about the steps
     def preprocess_data(self, cleaned_data: pd.DataFrame, **kwargs) -> pd.DataFrame:
+        """Preprocess the data.
+        Impute missing values.
+        Scale/transform values.
+        Should put the prediction target in a column named "outcome"
+
+        Parameters
+        ----------
+        cleaned_data: pd.DataFrame
+        kwargs: dict
+            Dictionary of hyperparameters specifying judgement calls
+
+        Returns
+        -------
+        preprocessed_data: pd.DataFrame
+        """
+
         tbi_df = cleaned_data.copy()
         ################################
         # Step 1: Remove variables which have nothing to do with our problem (uncontroversial choices)
@@ -72,8 +105,9 @@ class Dataset(DatasetTemplate):
         ################################
         # Step 4: Remove Missing Observations Among the Response Outcomes
         ################################
+
         tbi_df_step4 = tbi_df_step3.dropna(subset=['DeathTBI', 'Intub24Head', 'Neurosurgery', 'HospHead'])
-        tbi_df_step4['PosIntFinal'].fillna(0, inplace=True)
+        tbi_df_step4.loc[:, "PosIntFinal"].fillna(0, inplace=True)
 
         ################################
         # Step 5: Make a New Column ciTBI, without the hospitalization condition
@@ -86,24 +120,39 @@ class Dataset(DatasetTemplate):
 
 
 
-        # TODO
+        # TODO:
 
-        # drop cols with vals missing this percent of the time
-        df = cleaned_data.dropna(axis=1, thresh=(1 - kwargs['frac_missing_allowed']) * cleaned_data.shape[0])
+        # # drop cols with vals missing this percent of the time
+        # df = cleaned_data.dropna(axis=1, thresh=(1 - kwargs['frac_missing_allowed']) * cleaned_data.shape[0])
 
-        # impute missing values
-        # fill in values for some vars from unknown -> None
-        df.loc[df['AbdomenTender'].isin(['no', 'unknown']), 'AbdTenderDegree'] = 'None'
+        # # impute missing values
+        # # fill in values for some vars from unknown -> None
+        # df.loc[df['AbdomenTender'].isin(['no', 'unknown']), 'AbdTenderDegree'] = 'None'
 
-        # pandas impute missing values with median
-        df = df.fillna(df.median())
-        df.GCSScore = df.GCSScore.fillna(df.GCSScore.median())
+        # # pandas impute missing values with median
+        # df = df.fillna(df.median())
+        # df.GCSScore = df.GCSScore.fillna(df.GCSScore.median())
 
-        df['outcome'] = df[self.get_outcome_name()]
+        # df['outcome'] = df[self.get_outcome_name()]
 
-        return df
+        return tbi_df_step5
 
     def extract_features(self, preprocessed_data: pd.DataFrame, **kwargs) -> pd.DataFrame:
+        """Extract features from preprocessed data
+        All features should be binary
+
+
+        Parameters
+        ----------
+        preprocessed_data: pd.DataFrame
+        kwargs: dict
+            Dictionary of hyperparameters specifying judgement calls
+
+        Returns
+        -------
+        extracted_features: pd.DataFrame
+        """
+
         # TODO
         # add engineered featuures
         df = helper.derived_feats(preprocessed_data)
@@ -133,16 +182,41 @@ class Dataset(DatasetTemplate):
         return df[feats]
 
     def get_outcome_name(self) -> str:
+        """Returns the name of the outcome we are predicting
+        """
+
         return 'PosIntFinal'  # return the name of the outcome we are predicting
 
     def get_dataset_id(self) -> str:
+        """Returns the name of the dataset id (str)
+        """
+
         return 'tbi_pecarn'  # return the name of the dataset id
 
     def get_meta_keys(self) -> list:
+        """Returns a list of keys which should not be used in fitting but are still useful for analysis
+        """
+
         # TODO
         return []  # keys which are useful but not used for prediction
 
     def get_judgement_calls_dictionary(self) -> Dict[str, Dict[str, list]]:
+        """Returns a dictionary of keyword arguments for each function in the dataset class.
+        Each key should be a string with the name of the arg.
+        Each value should be a list of values, with the default value coming first.
+
+        Example
+        -------
+        return {
+            'clean_data': {},
+            'preprocess_data': {
+                'imputation_strategy': ['mean', 'median'],  # first value is default
+            },
+            'extract_features': {},
+        }
+        """
+
+
         return {
             'clean_data': {},
             'preprocess_data': {
@@ -154,6 +228,33 @@ class Dataset(DatasetTemplate):
                 'drop_negative_columns': [False],  # default value comes first
             },
         }
+
+    #NOTE: for quick reference - this is what's inherited and gets run:
+
+    # def get_data(self, save_csvs: bool = False,
+    #              data_path: str = rulevetting.DATA_PATH,
+    #              load_csvs: bool = False,
+    #              run_perturbations: bool = False) -> (pd.DataFrame, pd.DataFrame, pd.DataFrame):
+    #     """Runs all the processing and returns the data.
+    #     This method does not need to be overriden.
+
+    #     Params
+    #     ------
+    #     save_csvs: bool, optional
+    #         Whether to save csv files of the processed data
+    #     data_path: str, optional
+    #         Path to all data
+    #     load_csvs: bool, optional
+    #         Whether to skip all processing and load data directly from csvs
+    #     run_perturbations: bool, optional
+    #         Whether to run / save data pipeline for all combinations of judgement calls
+
+    #     Returns
+    #     -------
+    #     df_train
+    #     df_tune
+    #     df_test
+    #     """
 
 
 if __name__ == '__main__':
