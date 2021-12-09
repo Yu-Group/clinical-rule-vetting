7d22cda1a647b23c86924185c6fe85c833cf27b1
Seunghoon Paik <shpaik@berkeley.edu>
2021-12-09 12:12:23 -0800

    SH: code trimming

--- rulevetting/projects/csi_pecarn/dataset.py
+++ rulevetting/projects/csi_pecarn/dataset.py
@@ -8,9 +8,8 @@ from typing import Dict
 
 import rulevetting
 import rulevetting.api.util
-from rulevetting.projects.iai_pecarn import helper
-from rulevetting.templates.dataset import DatasetTemplate
-
+from rulevetting.projects.csi_pecarn import helper
+from rulevetting.templates.dataset_new import DatasetTemplate
 
 class Dataset(DatasetTemplate):
     def clean_data(self, data_path: str = rulevetting.DATA_PATH, **kwargs) -> pd.DataFrame:
@@ -18,12 +17,9 @@ class Dataset(DatasetTemplate):
         os.makedirs(raw_data_path, exist_ok=True)
 
         # all the fnames to be loaded and searched over
-        
         fnames = sorted([
             fname for fname in os.listdir(raw_data_path)
             if 'csv' in fname
-            #   and not 'radiology' in fname
-            #   and not 'kappa' in fname
             ])
 
         # read through each fname and save into the r dictionary
@@ -51,10 +47,11 @@ class Dataset(DatasetTemplate):
                             and not 'kappa' in fname
                             and not 'injuryclassification' in fname
                             and not 'outside' in fname
-                            and not 'onfield' in fname]
+                            and not 'onfield' in fname
+                            and not 'medicalhistory' in fname]
 
         df_features = r[fnames_small[0]]
-        print('merge all the dfs...')
+        print('merge all the dfs...', fnames_small)
         for i, fname in tqdm(enumerate(fnames_small)):
             df2 = r[fname].copy()
             
@@ -63,81 +60,91 @@ class Dataset(DatasetTemplate):
             
             # don't save duplicate columns
             df_features = df_features.set_index('SubjectID').combine_first(df2.set_index('SubjectID')).reset_index()
-
-        df_outcomes = helper.get_outcomes(raw_data_path)
-
-        df = pd.merge(df_features, df_outcomes, on='id', how='left')
-        df = helper.rename_values(df)  # rename the features by their meaning
-
+        
+        # -----
+        var_1 = ['AVPUDetails', 'AgeInYears', 'AlteredMentalStatus', 'ArrPtIntub', 'Assault',
+                 'AxialLoadAnyDoc', 'CaseID', 'CervicalSpineImmobilization', 'ChildAbuse', 'ControlType',
+                 'DxCspineInjury', 'Ethnicity', 'FallDownStairs', 'FallFromElevation', 'FocalNeuroFindings',
+                 'Gender', 'HeadFirst', 'HighriskDiving', 'HighriskFall', 'HighriskHanging',
+                 'HighriskHitByCar', 'HighriskMVC', 'HighriskOtherMV', 'InjuryPrimaryMechanism', 'IntervForCervicalStab',
+                 'LOC', 'LimitedRangeMotion','LongTermRehab', 'Predisposed', "MedsGiven",
+                 "MedsRecdPriorArrival", "MotorGCS", "PainNeck", "PainNeck2", "PassRestraint",
+                 "PosMidNeckTenderness", 'PosMidNeckTenderness2',"PtAmbulatoryPriorArrival", "PtCompPain"] # 39
+        
+        var_2 = ['PtCompPainHead', 'PtCompPainNeck', 'PtCompPainPelvis', 'PtExtremityWeakness', 'PtParesthesias',
+                 'PtSensoryLoss', 'PtTender', 'PtTenderAbd', 'PtTenderBack', 'PtTenderChest',
+                 'PtTenderExt', 'PtTenderFlank', 'PtTenderHead', 'PtTenderNeck', 'PtTenderPelvis',
+                 'ShakenBabySyndrome', 'SiteID', 'SubInj_Ext', 'SubInj_Face', 'SubInj_Head',
+                 'SubInj_TorsoTrunk', 'SubjectID', 'TenderNeck', 'Torticollis', 'TotalGCS',
+                 'ambulatory', 'axialloadtop', 'helmet'] # 28
+        
+        var_use = var_1 + var_2
+        
+        df = df_features[var_use]        
+        df = df.drop(columns=['PosMidNeckTenderness2','PainNeck2','PtAmbulatoryPriorArrival','HeadFirst','PtCompPainNeck',
+                              'PtTenderNeck','TotalGCS'])
+        df = helper.rename_values(df)  
+        
         return df
 
     def preprocess_data(self, cleaned_data: pd.DataFrame, **kwargs) -> pd.DataFrame:
-
+        
+        df = cleaned_data
+    
+        ### code for only using random control
+        '''
+        ind1=[True for i in range(cleaned_data.shape[0])]
+        index=pd.array(ind1,dtype='boolean')
+        for i in range(len(index)):
+            if df.ControlType[i]=='case' or  df.ControlType[i]=='ran':
+                index[i]=True
+            else:
+                index[i]=False
+        df=df[index]
+        '''
+        
+        #df=df[(df.ControlType == 'case') or (df.ControlType == 'ran')]
+        df = df.assign(outcome=lambda x: (x.ControlType == 'case').astype(int))
+        
         # drop cols with vals missing this percent of the time
-        df = cleaned_data.dropna(axis=1, thresh=(1 - kwargs['frac_missing_allowed']) * cleaned_data.shape[0])
+        df = df.dropna(axis=1, thresh=(1 - kwargs['frac_missing_allowed']) * df.shape[0])
+        df = df.drop(columns=['AgeInYears','InjuryPrimaryMechanism'])
+       
+        # impute missing values with median
+        df = df.fillna(df.median())  
+        
+        #df['outcome'] = df[self.get_outcome_name()]
+        #df = df.assign(outcome=lambda x: (x.ControlType == 'case').astype(int))
+        
+        return df
+        
 
-        # impute missing values
-        # fill in values for some vars from unknown -> None
-        df.loc[df['AbdomenTender'].isin(['no', 'unknown']), 'AbdTenderDegree'] = 'None'
-
-        # pandas impute missing values with median
-        df = df.fillna(df.median())
-        df.GCSScore = df.GCSScore.fillna(df.GCSScore.median())
-
-        df['outcome'] = df[self.get_outcome_name()]
-
+    def extract_features(self, preprocessed_data: pd.DataFrame, **kwargs) -> pd.DataFrame: 
+        df = preprocessed_data
         return df
 
-    def extract_features(self, preprocessed_data: pd.DataFrame, **kwargs) -> pd.DataFrame:
-        # add engineered featuures
-        df = helper.derived_feats(preprocessed_data)
-
-        # convert feats to dummy
-        df = pd.get_dummies(df, dummy_na=True)  # treat na as a separate category
-
-        # remove any col that is all 0s
-        df = df.loc[:, (df != 0).any(axis=0)]
-
-        # remove the _no columns
-        if kwargs['drop_negative_columns']:
-            df.drop([k for k in df.keys() if k.endswith('_no')], inplace=True)
-
-        # narrow to good keys
-        feat_names = [k for k in df.keys()  # features to use
-                      if not 'iai' in k.lower()]
-        base_feat_names = []
-        base_feat_names += ['AbdDistention', 'AbdTenderDegree', 'AbdTrauma', 'AbdTrauma_or_SeatBeltSign',
-                            'AbdomenPain', 'Costal', 'DecrBreathSound', 'DistractingPain',
-                            'FemurFracture', 'GCSScore', 'Hypotension', 'LtCostalTender',
-                            'MOI', 'RtCostalTender', 'SeatBeltSign', 'ThoracicTender',
-                            'ThoracicTrauma', 'VomitWretch', 'Age', 'Sex']
-        base_feat_names += self.get_meta_keys()
-        feats = rulevetting.api.util.get_feat_names_from_base_feats(feat_names,
-                                                                    base_feat_names=base_feat_names) + ['outcome']
-        return df[feats]
-
-    def get_outcome_name(self) -> str:
-        return 'iai_intervention'  # return the name of the outcome we are predicting
+    # def get_outcome_name(self) -> str:
+    #    return 'csi_intervention'  # return the name of the outcome we are predicting
 
     def get_dataset_id(self) -> str:
-        return 'iai_pecarn'  # return the name of the dataset id
+        return 'csi_pecarn'  # return the name of the dataset id
 
     def get_meta_keys(self) -> list:
-        return ['Race', 'InitHeartRate', 'InitSysBPRange']  # keys which are useful but not used for prediction
-
+        return ["SubjectID", "SiteID", "CaseID", "ControlType", "Ethnicity", "Gender"]
+        # keys which are useful but not used for prediction
+        
     def get_judgement_calls_dictionary(self) -> Dict[str, Dict[str, list]]:
         return {
             'clean_data': {},
             'preprocess_data': {
                 # drop cols with vals missing this percent of the time
-                'frac_missing_allowed': [0.05, 0.10],
+                'frac_missing_allowed': [0.15, 0.05],
             },
             'extract_features': {
                 # whether to drop columns with suffix _no
                 'drop_negative_columns': [False],  # default value comes first
             },
         }
-
 
 if __name__ == '__main__':
     dset = Dataset()
---
+++ rulevetting/projects/csi_pecarn/model_best.py
@@ -0,0 +1,2 @@
+# out models are all implemented in model_helper.py file
+# in model_helper.py file, there are more functions for the other models as well
