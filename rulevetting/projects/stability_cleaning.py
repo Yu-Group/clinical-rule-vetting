16bac4d5f305ee28e6ee3b57abaafdd612b4f3c2
OmerRonen <omerronen10@gmail.com>
2022-11-04 13:33:25 -0700

    results

--- rulevetting/projects/stability_cleaning.py
+++ rulevetting/projects/stability_cleaning.py
@@ -17,37 +17,64 @@ def _get_x_y(ds, columns):
     X = X.loc[:, columns]
     return X, y
 
-def get_specificity(y_true, y_prob_pred, tpr_min):
+
+def get_sensitivity_thres(y_true, y_prob_pred, tpr_min):
     roc = roc_curve(y_true=y_true, y_score=y_prob_pred)
+
+    # fpr = roc[0]
     tpr = roc[1]
-    fpr = roc[0]
+    thres = roc[2]
 
     indx = np.where(tpr >= tpr_min)[0][0]
-    return 1-fpr[indx]
+    return thres[indx]
 
 
 if __name__ == '__main__':
+    tpr = 98
     if not os.path.exists("/accounts/campus/omer_ronen/projects/rule-vetting/data/tbi_pecarn/processed/perturbed_data"):
         tbiDataset().get_data(run_perturbations=True, load_csvs=False, save_csvs=True)
     data = tbiDataset().get_data(run_perturbations=True, load_csvs=True, save_csvs=True)
     columns = list(set.intersection(*[set(d[0].columns) for d in list(data.values())]))
     columns.remove("outcome")
     models = []
+
+
+    def _get_perturb_name(perturb):
+        s = perturb.find("[") + 1
+        e = perturb.find("]")
+        return perturb[s:e].replace("'", "")
+
+
     for perturb, (train, tune, test) in data.items():
+        print(_get_perturb_name(perturb))
+        print(f"Shapes: train - {train.shape[0]}, tune - {tune.shape[0]}, test - {test.shape[0]}")
+        _, y_test = _get_x_y(test, columns)
+        print(f"test hist: 1: {np.sum(y_test == 1)}, 0: {np.sum(y_test == 0)}")
+
         mdl = RuleFitClassifier()
         X_train, y_train = _get_x_y(train, columns)
-        if len(y_train) == 0:
-            continue
+        X_tune, y_tune = _get_x_y(tune, columns)
 
+        #
         mdl.fit(X_train, y_train)
-        models.append((perturb, mdl))
-    i = 0
-    models_spec = {perturb: [] for perturb in data.keys()}
+        thres = get_sensitivity_thres(y_tune, mdl.predict_proba(X_tune)[:, 1], tpr_min=0.01 * tpr)
+
+        models.append((perturb, mdl, thres))
+    # i = 0
+
+    models_spec = {_get_perturb_name(perturb): [] for perturb in data.keys()}
+    models_sens = {_get_perturb_name(perturb): [] for perturb in data.keys()}
+
     for perturb, (train, tune, test) in data.items():
         X_test, y_test = _get_x_y(test, columns)
-        for p, mdl in enumerate(models):
-            y_pred = mdl.predict_proba(X_test)[:, 1]
-            spec = get_specificity(y_test, y_pred, tpr_min=0.98)
-            models_spec[p].append(spec)
-    print(models_spec)
-    pd.DataFrame(models_spec).to_csv(f"/accounts/campus/omer_ronen/projects/rule-vetting/results/tbi_98.csv")
+        for p, mdl, thres in models:
+            y_pred = np.array(mdl.predict_proba(X_test)[:, 1] > thres, dtype=np.int)
+            sensitivity = np.sum(y_test[y_pred == 1]) / np.sum(y_test)
+            specificty = np.sum((1 - y_test)[y_pred == 0]) / np.sum((1 - y_test))
+            # spec = get_specificity(y_test, y_pred, tpr_min=0.01*tpr)
+            models_spec[_get_perturb_name(p)].append(specificty)
+            models_sens[_get_perturb_name(p)].append(sensitivity)
+
+    # print(models_spec)
+    pd.DataFrame(models_spec).to_csv(f"/accounts/campus/omer_ronen/projects/rule-vetting/results/tbi_spec_{tpr}.csv")
+    pd.DataFrame(models_sens).to_csv(f"/accounts/campus/omer_ronen/projects/rule-vetting/results/tbi_sens_{tpr}.csv")
--- rulevetting/projects/tbi_pecarn/dataset.py
+++ rulevetting/projects/tbi_pecarn/dataset.py
@@ -14,6 +14,8 @@ import numpy as np
 import pandas as pd
 from joblib import Memory
 
+import matplotlib.pylab as plt
+
 import rulevetting
 from rulevetting.projects import one_hot_encode_df
 from rulevetting.projects.tbi_pecarn import helper
@@ -129,6 +131,15 @@ class Dataset:
                                                                                            'DeathTBI']].sum(
                 axis=1) >= 1).astype(int)
 
+            LOGGER.info(f"Data shape: {cleaned_data.shape}\nciTBI: {cleaned_data['PosIntFinal'].value_counts()/cleaned_data.shape[0]}")
+            na_sum = cleaned_data.isna().sum()
+            na_sum = na_sum[na_sum > 100]
+            na_sum.reset_index(name="n").plot.bar(x='index', y='n', rot=60, fontsize=10, legend=None, figsize=(12,12))
+            plt.ylabel("Number of Missing Values")
+            plt.savefig("/accounts/campus/omer_ronen/projects/rule-vetting/results/na.png", dpi=300)
+            plt.close()
+            raise RuntimeError("Pick")
+
         # judgement call - we drop patients with gcs <14 and thus gcs scores
         if kwargs['drop_low_gcs']:
             cleaned_data = cleaned_data.loc[cleaned_data['GCSTotal'] >= 14, :]
@@ -136,7 +147,17 @@ class Dataset:
             gcs_vars = ['GCSEye', 'GCSMotor', 'GCSVerbal']
             cleaned_data = cleaned_data.drop(columns=gcs_vars)
 
+        # cleaned_data = cleaned_data.replace('Not applicable', np.nan)
+        null_vec = cleaned_data.isnull().sum(axis=0)
+
+        null_vec = null_vec[null_vec != 0]
+
         if not kwargs['propensity']:
+            def _drop_nas(col):
+                for c in kwargs['drop nas']:
+                    if col.startswith(c):
+                        return True
+                return False
 
             # judgement call - impute unknowns with mode/mean/or drop patient
             if 'impute_unknowns' in kwargs:
@@ -144,7 +165,8 @@ class Dataset:
                     col_data = cleaned_data.loc[:, col]
                     not_nan_idx = col_data == col_data
                     nas = np.mean(not_nan_idx)
-                    if col not in kwargs['drop nas']:
+                    # if col not in kwargs['drop nas']:
+                    if not _drop_nas(col):
 
                         mode_fill = cleaned_data.loc[:, col].fillna(col_data.mode()[0])
                         n_row = len(mode_fill)
@@ -262,10 +284,14 @@ class Dataset:
         np.random.seed(42)
         indices = np.random.choice(n, size=n, replace=False)
         # LOGGER.info(indices[0:5])
-        pre_indices = list(preprocessed_data.index)
-        train_ind = [i for i in indices[0:int(0.6*n)] if i in pre_indices]
-        tune_ind = [i for i in indices[int(0.6*n):int(0.8*n)] if i in pre_indices]
-        test_ind = [i for i in indices[int(0.8*n):n]if i in pre_indices]
+        pre_indices = set(preprocessed_data.index)
+        train_ind = list(set(indices[0:int(0.6*n)]).intersection(pre_indices))
+        tune_ind = list(set(indices[int(0.6*n):int(0.8*n)]).intersection(pre_indices))
+        test_ind = list(set(indices[int(0.8*n):n]).intersection(pre_indices))
+
+        # train_ind = [i for i in indices[0:int(0.6*n)] if i in pre_indices]
+        # tune_ind = [i for i in indices[int(0.6*n):int(0.8*n)] if i in pre_indices]
+        # test_ind = [i for i in indices[int(0.8*n):n]if i in pre_indices]
 
         return (
             preprocessed_data.loc[train_ind, :],
@@ -392,9 +418,7 @@ class Dataset:
                 'impute_unknowns': ['mode'],
                 'impute_not_applicables': [False],
                 'propensity': [False],
-                "drop nas": [list(l) for l in powerset(["AMS", "Hema", "OSI",
-                                                        "SFxPalp","Clav"
-                                                        "SFxBas"])]
+                "drop nas": [list(l) for l in powerset(["HASeverity", "LocLen", "LOCSeparate"])]
 
             },
             'extract_features': {}
